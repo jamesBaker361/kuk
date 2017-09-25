73c25a9f3a085e88b3774780eef6db9b653f7fce
jamesBaker361 <jlbaker361@gmail.com>
2017-09-25 18:22:32 -0400

    strict is fucking dumb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 /*request.post({
     url: "https://api.kik.com/v1/config",
@@ -19,19 +19,14 @@
 
 });*/
 
-let util = require('util');
-let http = require('http');
-let Bot  = require('@kikinteractive/kik');
-let express=require('express');
-
-let app=express();
+var util = require('util'),http = require('http'),Bot  = require('@kikinteractive/kik'),express=require('express'),app=express();
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'index.html');
 })
 
 // Configure the bot API endpoint, details for your bot
-let bot = new Bot({
+var bot = new Bot({
     username: 'stornettatron3000',
     apiKey: '973c4a39-a9dd-4cd1-86c4-2701f1e8ac39',
     baseUrl: 'http://kuk-drkikbot.193b.starter-ca-central-1.openshiftapps.com/'
@@ -45,7 +40,5 @@ bot.onTextMessage((message) => {
 });
 
 // Set up your server and start listening
-let server = http
-    .createServer(bot.incoming())
-    .listen(process.env.PORT || 8080);
+//let server = http.createServer(bot.incoming()).listen(process.env.PORT || 8080);
 
