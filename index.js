2343dab137729fff2157a43587f04cc1da0ce5ce
jamesBaker361 <jlbaker361@gmail.com>
2017-09-25 18:25:15 -0400

    yeet

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-
-
-/*request.post({
-    url: "https://api.kik.com/v1/config",
-    auth: {
-        user: "stornettatron3000",
-        pass: "973c4a39-a9dd-4cd1-86c4-2701f1e8ac39"
-    },
-    json: {
-        "webhook": "http://kuk-drkikbot.193b.starter-ca-central-1.openshiftapps.com/", 
-        "features": {
-            "receiveReadReceipts": false, 
-            "receiveIsTyping": false, 
-            "manuallySendReadReceipts": false, 
-            "receiveDeliveryReceipts": false
-        }
-    }
-}, function(){
-
-});*/
-
 var util = require('util'),http = require('http'),Bot  = require('@kikinteractive/kik'),express=require('express'),app=express();
 
 app.get('/',function(req,res){
@@ -29,7 +8,7 @@ app.get('/',function(req,res){
 var bot = new Bot({
     username: 'stornettatron3000',
     apiKey: '973c4a39-a9dd-4cd1-86c4-2701f1e8ac39',
-    baseUrl: 'http://kuk-drkikbot.193b.starter-ca-central-1.openshiftapps.com/'
+    baseUrl: 'http://kikdick-kikdick.193b.starter-ca-central-1.openshiftapps.com'
 });
 
 bot.updateBotConfiguration();
